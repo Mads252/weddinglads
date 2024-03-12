650ebb78f8e5a680751c7885427080503e1241ab
Mads Toft Eriksen <mads1234@live.com>
2024-03-12 10:17:19 +0100

    slider

--- app.js
+++ app.js
@@ -41,3 +41,33 @@ window.addEventListener("load", () => {
     document.body.removeChild(loader);
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("billeder_slides");
+  let dots = document.getElementsByClassName("prik");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" aktiv_prik", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " aktiv_prik";
+  setTimeout(showSlides, 3000); 
+}
