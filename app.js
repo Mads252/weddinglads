f5c7d1033b46245c4d56a9d505f2fb34844b149d
Mads Toft Eriksen <mads1234@live.com>
2024-03-14 20:44:35 +0100

    fghj

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
 var elements = document.getElementsByClassName('countdown');
-
-
 var countDownDate = new Date("Mar 22, 2024 00:00:00").getTime();
-
-
 var x = setInterval(function() {
-
-
   var now = new Date().getTime();
-
-
   var distance = countDownDate - now;
-
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
- 
+  // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   for (var i = 0; i < elements.length; i++) {
     elements[i].innerHTML = + days + " dage til dit bryllup ";
   }
-
-
   if (distance < 0) {
     clearInterval(x);
     for (var i = 0; i < elements.length; i++) {
-      elements[i].innerHTML = "EXPIRED";
+      elements[i].innerHTML = "BRYLLUP";
     }
   }
 }, 1000);
+
+
+
 
 window.addEventListener("load", () => {
   const loader = document.querySelector(".loader");
